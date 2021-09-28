b7d310ce42ccd88207a76db7473070856ab0a64c
Jesse Hoobergs <jesse@codebergs.com>
2021-09-28 14:21:38 +0200

    Tmp

--- erd/src/draw.rs
+++ erd/src/draw.rs
@@ -128,7 +128,7 @@ impl Point {
 #[derive(Debug, Clone, Copy)]
 pub struct Circle {
     center: Point,
-    radius: isize,
+    radius: usize,
 }
 
 impl Circle {
@@ -143,6 +143,29 @@ impl Circle {
         }
     }
 }
+
+#[derive(Debug, Clone, Copy)]
+pub struct Rectangle {
+    center: Point,
+    width: usize,
+    height: usize,
+}
+
+impl Rectangle {
+    /*fn overlaps_with(&self, other: &Ellipse) -> bool {
+        let distance = self.center.distance(&other.center);
+        distance < (self.radius + other.radius) as f64
+    }*/
+    fn moved(&self, x: isize, y: isize) -> Self {
+        Self {
+            center: self.center.moved(x, y),
+            width: self.width,
+            height: self.height,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 
 pub trait Draw {
     fn radius(&self) -> isize;
@@ -205,6 +228,81 @@ impl Draw for EntityElement {
                 .map(|a| a.radius())
                 .max()
                 .unwrap_or(0)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct EntityGrid((String, Rectangle), Vec<(String, Ellipse)>);
+
+impl EntityGrid {
+    pub fn new(name: String, rect: Rectangle) -> Grid {
+        EntityGrid((name, rect), Vec::new())
+    }
+    pub fn add_circle(&mut self, radius: isize, name: String) {
+        if self.0.is_empty() {
+            self.0.push((
+                name,
+                Circle {
+                    center: Point { x: 0, y: 0 },
+                    radius,
+                },
+            ))
+        } else {
+            let mut new_circle = None;
+            for (_, circle) in self.0.iter() {
+                for (x_change, y_change) in vec![(1, 0), (0, 1), (-1, 0), (0, -1)] {
+                    let possible_center = circle.center.moved(
+                        x_change * (circle.radius + radius),
+                        y_change * (circle.radius + radius),
+                    );
+                    let possible_circle = Circle {
+                        center: possible_center,
+                        radius,
+                    };
+                    if self.can_add_circle(&possible_circle) {
+                        new_circle = Some(possible_circle);
+                        break;
+                    }
+                }
+                if new_circle.is_some() {
+                    break;
+                }
+            }
+            self.0
+                .push((name, new_circle.expect("Circle position not found")))
+        }
+    }
+    pub fn can_add_circle(&self, circle: &Circle) -> bool {
+        for (_, c) in self.0.iter() {
+            if c.overlaps_with(circle) {
+                return false;
+            }
+        }
+        return true;
+    }
+    // Moved grid so nothing is left or above coordinate 0
+    pub fn normalized(&self) -> NormalizedGrid {
+        if self.0.is_empty() {
+            return NormalizedGrid(Vec::new());
+        }
+        let min_x = self
+            .0
+            .iter()
+            .map(|(_, c)| c.center.x - c.radius)
+            .min()
+            .expect("Missing circles?");
+        let min_y = self
+            .0
+            .iter()
+            .map(|(_, c)| c.center.y - c.radius)
+            .min()
+            .expect("Missing circles?");
+        NormalizedGrid(
+            self.0
+                .iter()
+                .map(|(n, c)| (n.clone(), c.moved(-min_x, -min_y)))
+                .collect(),
+        )
     }
 }
 
