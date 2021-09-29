fed5e4594a21d62876f8891f1015510e3ca53172
Jesse Hoobergs <jesse@codebergs.com>
2021-09-29 22:44:54 +0200

    Add initial intro for a the book

---
+++ book/highlight-erd.js
@@ -0,0 +1,37 @@
+// Syntax highlighting for erd-script.
+
+hljs.registerLanguage("erd", function (hljs) {
+  // Basic syntax.
+  var comment = { className: "comment", begin: "//", end: /$/ };
+  var ident = {
+    className: "title",
+    begin: /[_a-zA-Z][_a-z0-9A-Z]*/,
+    end: /$/,
+  };
+  var special = {
+    begin:
+      /entity|attribute|id|relation|one|exactly|multiple|required|optional/,
+    className: "keyword",
+  };
+
+  // Escape sequences within a string or character literal.
+  var escape = { begin: /\\./ };
+
+  var string_ident = {
+    begin: /[_a-zA-Z][_a-z0-9A-Z]*/,
+    end: /$/,
+    contains: [
+      { className: "string", begin: /\(/, end: /\)/ }, //, contains: [escape] },
+    ],
+  };
+
+  return {
+    contains: [special, string_ident, ident, comment],
+  };
+});
+
+// This file is inserted after the default highlight.js invocation, which tags
+// unknown-language blocks with CSS classes but doesn't highlight them.
+Array.from(document.querySelectorAll("code.language-erd")).forEach(
+  hljs.highlightBlock
+);
---
+++ book/src/intro.md
@@ -0,0 +1,37 @@
+# Introduction
+
+`erd-script` is a system to easily draw Entity-Relation (ER) diagrams.
+
+In this case 'easily' means without needing to pay attention to the drawing part itself. This implies that the writer only needs to think about specifying the right entities, relations, attributes and identifiers.
+
+It uses `graphviz` to actually draw to diagrams.
+
+## Sample
+
+```erd
+// Modelled after https://graphviz.org/Gallery/undirected/ER.html
+
+entity course
+  attribute name
+  id code
+
+entity institute
+  attribute name
+
+entity student
+  attribute name
+  attribute number
+  attribute grade
+
+relation CI(C-I)
+  one required institute
+  multiple optional course
+
+relation SI(S-I)
+  one required institute
+  multiple required student
+
+relation SC(S-C)
+  multiple optional course
+  multiple optional student
+```
