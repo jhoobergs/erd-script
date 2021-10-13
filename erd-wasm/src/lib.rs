a8ee2e25b3bc5d05bdd5f283560b30e74cc1bb6b
Jesse Hoobergs <jesse@codebergs.com>
2021-10-13 16:31:23 +0200

    First version of erd-wasm physical

--- erd-wasm/src/lib.rs
+++ erd-wasm/src/lib.rs
@@ -1,5 +1,6 @@
 use erd_script::erd::ToDot;
 use serde::{Deserialize, Serialize};
+use std::convert::TryInto;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -25,18 +26,18 @@ impl ParsingError {
 }
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
-enum CompileError {
+enum ERDCompileError {
     ERDErrors(Vec<String>),
     ParsingError(ParsingError),
 }
 
-impl CompileError {
-    fn create(item: erd_script::erd::FromScriptError) -> Self {
+impl ERDCompileError {
+    fn create(item: erd_script::erd::ERDFromScriptError) -> Self {
         match item {
-            erd_script::erd::FromScriptError::ERDError(v) => {
+            erd_script::erd::ERDFromScriptError::ERDError(v) => {
                 Self::ERDErrors(v.into_iter().map(|a| format!("{}", a)).collect())
             }
-            erd_script::erd::FromScriptError::ParsingError(p) => {
+            erd_script::erd::ERDFromScriptError::ParsingError(p) => {
                 Self::ParsingError(ParsingError::create(p))
             }
         }
@@ -44,11 +45,48 @@ impl CompileError {
 }
 
 #[wasm_bindgen]
-pub fn compile(erd_script: &str) -> JsValue {
+pub fn compile_erd(erd_script: &str) -> JsValue {
     JsValue::from_serde(
         &erd_script::erd::ERD::from_script(erd_script)
             .map(|erd| erd.to_dot().to_string())
-            .map_err(CompileError::create),
+            .map_err(ERDCompileError::create),
+    )
+    .unwrap_or(false.into())
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+enum PhysicalCompileError {
+    ERDErrors(Vec<String>),
+    PhysicalErrors(Vec<String>),
+    ParsingError(ParsingError),
+}
+
+impl PhysicalCompileError {
+    fn create(item: erd_script::physical::PhysicalFromScriptError) -> Self {
+        match item {
+            erd_script::physical::PhysicalFromScriptError::ERDError(v) => {
+                Self::ERDErrors(v.into_iter().map(|a| format!("{}", a)).collect())
+            }
+            erd_script::physical::PhysicalFromScriptError::PhysicalError(v) => {
+                Self::PhysicalErrors(v.into_iter().map(|a| format!("{}", a)).collect())
+            }
+            erd_script::physical::PhysicalFromScriptError::ParsingError(p) => {
+                Self::ParsingError(ParsingError::create(p))
+            }
+        }
+    }
+}
+
+#[wasm_bindgen]
+pub fn compile_physical(erd_script: &str) -> JsValue {
+    JsValue::from_serde(
+        &erd_script::physical::PhysicalDescription::from_script(erd_script)
+            .map(|physical| {
+                let mut s = String::new();
+                physical.to_physical().write_sql_create(&mut s);
+                s
+            })
+            .map_err(PhysicalCompileError::create),
     )
     .unwrap_or(false.into())
 }
