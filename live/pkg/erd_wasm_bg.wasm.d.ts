ccfeab3e0f436387829740fc36ea8b9118d4bb8e
jhoobergs <jhoobergs@users.noreply.github.com>
2022-08-18 10:06:43 +0000

    Deploying to gh-pages from @ jhoobergs/erd-script@d553702744b167887f7bda985cc918bfd8a795e3 🚀

--- live/pkg/erd_wasm_bg.wasm.d.ts
+++ live/pkg/erd_wasm_bg.wasm.d.ts
@@ -1,6 +1,7 @@
 /* tslint:disable */
 /* eslint-disable */
 export const memory: WebAssembly.Memory;
-export function compile(a: number, b: number): number;
+export function compile_erd(a: number, b: number): number;
+export function compile_physical(a: number, b: number, c: number, d: number): number;
 export function __wbindgen_malloc(a: number): number;
 export function __wbindgen_realloc(a: number, b: number, c: number): number;
